d0e8fa2cc2466c078dc0816a270415f560c85326
iven germonpre <iven.germonpre@gmail.com>
2023-10-11 21:33:49 +0200

    some cleanup

--- src/main/java/be/dailysurvival/webserver/ConfigManager.java
+++ src/main/java/be/dailysurvival/webserver/ConfigManager.java
@@ -6,41 +6,51 @@ import org.bukkit.configuration.file.YamlConfiguration;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.logging.Logger;
 
 public class ConfigManager {
     private static File file;
     private static FileConfiguration fileConfiguration;
-    public static void setup(){
+    private static Logger logger = Bukkit.getLogger();
+
+    public static void setup() {
         file = new File(Bukkit.getServer().getPluginManager().getPlugin("WebServer").getDataFolder(), "WebServerConfig.yml");
-        if(!file.exists()){
-            System.out.println("File does not exist");
-            try{
-                file.createNewFile();
-            } catch (IOException e) {
-                System.out.println(e);
-            }
-            fileConfiguration = YamlConfiguration.loadConfiguration(file);
-            fileConfiguration.addDefault("port",80);
-            fileConfiguration.addDefault("WebSiteFolder","Website");
-            fileConfiguration.options().copyDefaults(true);
-            save();
+
+        // Check if the config file exists; if not, create it with default values
+        if (!file.exists()) {
+            logger.warning("WebServerConfig.yml does not exist. Creating default configuration...");
+            createDefaultConfig();
         }
+
+        fileConfiguration = YamlConfiguration.loadConfiguration(file);
     }
 
-
-
+    /**
+     * Creates a default configuration file.
+     */
+    private static void createDefaultConfig() {
+        try {
+            file.createNewFile();
+            fileConfiguration = YamlConfiguration.loadConfiguration(file);
+            fileConfiguration.addDefault("port", 80);
+            fileConfiguration.addDefault("WebSiteFolder", "Website");
+            fileConfiguration.addDefault("LogNewConnections",false);
+            fileConfiguration.options().copyDefaults(true);
+            save();
+        } catch (IOException e) {
+            logger.severe("Error while creating WebServerConfig.yml: " + e.getMessage());
+        }
+    }
 
     public static FileConfiguration get() {
         return fileConfiguration;
     }
 
-    public static void save(){
+    public static void save() {
         try {
             fileConfiguration.save(file);
-        }catch (IOException e){
-            System.out.println("Can not save the config file");
+        } catch (IOException e) {
+            logger.severe("Error while saving WebServerConfig.yml: " + e.getMessage());
         }
-
     }
-
 }
